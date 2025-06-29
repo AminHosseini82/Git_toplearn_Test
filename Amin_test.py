a2401df8c8961b849eec84203dc0a0f5667c5a49
Amin Hosseini <aminhosseini822003@gmail.com>
2025-06-29 21:55:54 +0330

    just a test22

--- Amin_test.py
+++ Amin_test.py
@@ -3,6 +3,6 @@ print("hii")
 for i in range(10):
     print(i)
 
-
-# this is Test 2 for testing Branch Functions
-print("this is beanch ")
+#
+# # this is Test 2 for testing Branch Functions
+# print("this is beanch ")
